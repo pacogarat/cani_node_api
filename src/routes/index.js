b0c978afacae9aae7efcddaac173a6f913fcc546
pacogarat <f.garat@sumauto.com>
2023-05-19 12:48:41 +0200

    añadida api externa

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,28 +2,62 @@ const { Router } = require('express');
 const router = Router();
 const { MongoClient, ObjectId } = require('mongodb');
 const uri = 'mongodb://localhost:27017/';
+const externalUri = 'https://645c042799b618d5f32be872.mockapi.io/ej2/message';
 const client = new MongoClient(uri);
 
 const colMessages = client.db("local").collection('messages');
-const getMessage = async (message_id) => {
-    const message = await colMessages.findOne({_id: new ObjectId(message_id)});
+const getMessage = (message_id) => {
+    console.log("Fetching doc "+message_id+" from mongoDB");
+    const message = colMessages.findOne({_id: new ObjectId(message_id)});
     return message;
 }
 
 const deleteMessage = async (message_id) => {
-    const result = await colMessages.deleteOne({_id: new ObjectId(message_id)});
+    console.log("Deleting doc "+message_id+" from mongoDB");
+    message = await getMessage(message_id);
+    const result = colMessages.deleteOne({_id: new ObjectId(message_id)});
+    if (message !== null && message.external_id !== null) {
+        console.log("Deleting doc with external id \""+message.external_id+"\" from external API");
+        await deleteAPIMock(message.external_id);
+    }
+
     return result;
 }
 const insertMessage = async (message) => {
-    const result = await colMessages.insertOne(message);
+    console.log("Inserting doc "+message.text+" into API");
+    const message_api = await (await insertAPIMock(message.text)).json();
+    console.debug("result api");
+    console.debug(message_api);
+    message.external_id = message_api.id;
+    const result = colMessages.insertOne(message);
     return result;
 }
-const updateMessage = async (message) => {
-    console.debug('1');
-    const result = await colMessages.updateOne({_id: message._id}, { $set: {text: message.text}});
 
-    console.debug('2');
-    return result;
+const insertMessages = async (messages) => {
+    console.log("Creating "+messages.length+" docs from API to insert into MongoDB");
+    var messagesToInsert = [];
+    for (let i=0; i<messages.length; i++) {
+        console.log("Creating document "+i);
+        const message = {
+            external_id:    messages[i].id,
+            text:           messages[i].message
+        };
+        messagesToInsert.push(message);
+    }
+    console.log("Inserting "+messages.length+" documents into MongoDB");
+    colMessages.insertMany(messagesToInsert);
+
+    return messagesToInsert;
+}
+const updateMessage = async (message) => {
+    console.log("Updating document "+message._id);
+    const result = colMessages.updateOne({_id: message._id}, { $set: {text: message.text}});
+    message = await getMessage(message._id);
+    if (message !== null && message.external_id !== null) {
+        console.log("Updating doc with external id \""+message.external_id+"\" from external API");
+        updateAPIMock(message.external_id, message.text);
+    }
+    return message;
 }
 class ValidationError extends Error {
     constructor(message) {
@@ -31,11 +65,61 @@ class ValidationError extends Error {
         this.name = "ValidationError";
     }
 }
+
+const getAllMessagesFromAPIMock = () => {
+    console.log("Fetching API data");
+    return fetch(externalUri);
+}
+const deleteAPIMock = (external_id) => {
+    console.log("Deleting API doc");
+    return fetch(externalUri + "/" + external_id, { method: 'DELETE' });
+}
+const updateAPIMock = (external_id, text) => {
+    console.log("Updating API doc with id: "+external_id);
+    return fetch(externalUri + "/" + external_id, {
+        method: 'PUT',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ "message": text })
+    });
+}
+const insertAPIMock = async (message) => {
+    console.log("Insert API doc with text: "+message.text);
+    return fetch(externalUri, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ "message": message.text })
+    });
+}
+
+const clearDB = () => {
+    console.log("Clearing BBDD");
+    return colMessages.deleteMany({});
+}
+
 const validateText = (text) => {
     if (text === null || (typeof text !== 'string' && !(text instanceof String))) throw new ValidationError("No se encontró el campo text");
     return text;
 }
+
 router
+    /**
+     * Borra lo que haya en bbdd inicialmente y carga los mensajes que se trae de la api externa en la bbdd
+     */
+    .get('/messages/init', async(req, res) => {
+        try {
+            const response = await getAllMessagesFromAPIMock();
+            var messages_api = await response.json();
+            await clearDB();
+            const messages = await insertMessages(messages_api);
+            res.status(200).json(messages);
+        } catch (e) {
+            res.status(500).json({errorMessage: e.message, stack: e.stack});
+        }
+    })
     .get('/message/:id', async(req, res) => {
         try {
             const message = await getMessage(req.params.id);
@@ -44,18 +128,23 @@ router
             else
                 res.status(404).json({errorMessage: "El documento no ha sido encontrado"});
         } catch (e) {
-            res.status(500).json({errorMessage: "Error"});
+            const statusCode = (e.name == "BSONError")?404:500;
+            res.status(500).json({errorMessage: e.message, stack: e.stack});
         }
     })
     .delete('/message/:id', async(req, res) => {
         try {
             const result = await deleteMessage(req.params.id);
+
             if (result.deletedCount > 0)
-                res.status(200).json({"Num documentos borrados": result.deletedCount})
+                res.status(200).json({
+                    "Num documentos borrados en mongo": result.deletedCount
+                })
             else
                 res.status(404).json({errorMessage: "Mensaje no encontrado"});
         } catch (e) {
-            res.status(500).json({errorMessage: "Error"});
+            const statusCode = (e.name === "BSONError")?400:500;
+            res.status(statusCode).json({errorMessage: e.message, stack: e.stack});
         }
     })
     .put('/message/:id', async(req, res) => {
@@ -67,10 +156,10 @@ router
                 text: text
             };
             const result = await updateMessage(message);
-            res.status(200).json();
+            res.status(200).json(result);
         } catch (e) {
-            const statusCode = (e instanceof ValidationError)? 400 : 500;
-            res.status(statusCode).json({error: true, message: "Ocurrió un error"});
+            const statusCode = (e instanceof ValidationError || e.name==="BSONError")? 400 : 500;
+            res.status(statusCode).json({errorMessage: e.message, stack: e.stack});
         }
 
     })
@@ -80,13 +169,12 @@ router
             const message = {
                 text: text
             };
-            console.debug("2");
             const result = await insertMessage(message);
-            console.debug("3");
             res.status(201).json(result);
         } catch(e) {
-            const statusCode = (e instanceof ValidationError)? 400 : 500;
-            res.status(statusCode).json({error: true, message: "Ocurrió un error"});
+            const statusCode = (e instanceof ValidationError || e.name==="BSONError")? 400 : 500;
+            res.status(statusCode).json({errorMessage: e.message, stack: e.stack});
         }
     })
+
 module.exports = router;
